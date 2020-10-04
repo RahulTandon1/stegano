148e2c4d68cd8c143bf31f0af2c111899e928643
Rahul Tandon <jesuisrahult@gmail.com>
2020-10-04 11:02:10 +0530

    first commit

---
+++ learning-stuff/RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; } // db pk stuff
+        public string Title { get; set; }
+
+        // ALERT!!!
+        // I have now clue what this line below does
+        [DataType(DataType.Date)] // makes stuff only date, no time stuff
+        public DateTime ReleaseData { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ learning-stuff/learning-c-sharp.md
@@ -0,0 +1,15 @@
+# stuff
+
+- `List<T>` = A collection, here a list, of the type `T`.
+- Entity framework is like Mongoose or Prisma but built by Microsoft for SQL...I think.
+- NuGet is the package manager for .NET stuff
+## TODOs: Figure the following stuff out
+
+- Classes
+- Interfaces
+- Enums // might be mini-classes
+- Delegates
+- Attributes
+- Async Code
+- LINQ
+- Arrow function thingie
