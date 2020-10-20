f9e8415e84f84149c1f4adc1854ad5a20dffc681
Rahul Tandon <jesuisrahult@gmail.com>
2020-10-20 08:20:11 +0530

    POC working

--- steganographyProj/steganographyProj/Controllers/HomeController.cs
+++ steganographyProj/steganographyProj/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Mime;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -41,8 +42,8 @@ namespace steganographyProj.Controllers
 
 
 
-        [HttpPost("FileUpload")]
-        public IActionResult upload(IFormFile file)
+        [HttpPost("encode")]
+        public IActionResult encode(IFormFile file, string plaintext)
         {
             //IFormFile file = files[0];
             if (file == null || file.Length == 0)
@@ -64,17 +65,54 @@ namespace steganographyProj.Controllers
 
             // std message
             // !!! Change to taking from user input.
-            string stdMsg = "aur ji pranam";
+            //string stdMsg = "aur ji pranam";
 
-            // process uploaded files
-            img = SteganographyHelper.embedText(stdMsg, img);
+            // hide message in bitmap
+            img = SteganographyHelper.embedText(plaintext, img);
 
+
+            // new memory stream for bitmap to png converion
             var stream = new MemoryStream();
-                        
+            //  
             img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
-                    var res = stream.ToArray();
+            //string format = MediaTypeNames.Application.Octet.ToString(); -> doing this leads to direct download
+            string format = "image/png";
+            stream.Seek(0, SeekOrigin.Begin);
+            FileStreamResult res= base.File(stream, format); ;
 
-                    return base.File(res, "result.png");
+            // adding png here does the trick. But maybe there's also a way of making it do the file extension
+            // stuff itself
+            res.FileDownloadName = "balleballe.png"; 
+            return res;
+
+        }
+        [HttpPost("decode")]
+        public IActionResult decode(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest();
+            }
+
+            // empty image to populate later
+            Bitmap img;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                file.CopyTo(memoryStream);
+
+                // making a new bitmap from uploaded image
+                Image tempImg = Image.FromStream(memoryStream);
+                img = (Bitmap)tempImg;
+            }
+
+
+            // hide message in bitmap
+            string plaintext = SteganographyHelper.extractText(img);
+
+
+            ViewData["plaintext"] = plaintext;
+            return View();
 
         }
     }
